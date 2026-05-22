331eea9df3ffe69c20915e711d2b9c9173722258
Juanito040 <eeuujuan954@gmail.com>
2026-05-22 16:46:59 -0500

    fix: agregar migracion automatica columna sysReporteIdFk en SysMovimientosStockRepuestos

--- NodeBackendProyectHusrt-biomedica-general/src/index.js
+++ NodeBackendProyectHusrt-biomedica-general/src/index.js
@@ -224,6 +224,9 @@ sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
   .then(() => sequelize.query(`
     ALTER TABLE \`SysRepuesto\` ADD COLUMN IF NOT EXISTS \`id_tipo_equipo_fk\` INTEGER NULL
   `).catch(() => {}))
+  .then(() => sequelize.query(`
+    ALTER TABLE \`SysMovimientosStockRepuestos\` ADD COLUMN IF NOT EXISTS \`sysReporteIdFk\` INTEGER NULL
+  `).catch(() => {}))
   .then(() => sequelize.query('SET FOREIGN_KEY_CHECKS = 1'))
   .then(() => sequelize.sync({ alter: false }))
   .then(() => {
