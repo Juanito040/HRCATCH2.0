11c5a0221145e2a48c224fbdd9878625065f27ea
Daniel <danie@dani.local>
2026-05-25 10:32:29 -0500

    fix: restringir edicion de repuestos a roles ADMIN (SISTEMASUSER/SYSTEMUSER solo lectura)

--- NodeBackendProyectHusrt-biomedica-general/routes/sistemas/sysRepuestoRoutes.js
+++ NodeBackendProyectHusrt-biomedica-general/routes/sistemas/sysRepuestoRoutes.js
@@ -4,7 +4,10 @@ const ctrl = require('../../controllers/Sistemas/sysRepuestoController');
 const requireRoles = require('../../utilities/requireRoles');
 const { checkToken } = require('../../utilities/middleware');
 
-const ROLES = ['SUPERADMIN', 'SISTEMASADMIN', 'SYSTEMADMIN', 'SISTEMASUSER', 'SYSTEMUSER'];
+// Solo administradores pueden crear/editar/deshabilitar
+const ADMIN_ROLES = ['SUPERADMIN', 'SISTEMASADMIN', 'SYSTEMADMIN'];
+// Usuarios con acceso de solo lectura
+// const READ_ROLES = ['SISTEMASUSER', 'SYSTEMUSER'];
 
 router.use(checkToken);
 
@@ -12,10 +15,10 @@ router.get('/', ctrl.getAll);
 router.get('/tipo/:id_tipo', ctrl.getByTipo);
 router.get('/usados/tecnico', ctrl.getUsadosPorTecnico);
 router.get('/:id', ctrl.getById);
-router.post('/', requireRoles(...ROLES), ctrl.create);
+router.post('/', requireRoles(...ADMIN_ROLES), ctrl.create);
 router.post('/descontar-stock', ctrl.descontarStock);
 router.post('/ajustar-stock-edicion', ctrl.ajustarStockEdicion);
-router.patch('/:id', requireRoles(...ROLES), ctrl.update);
-router.patch('/:id/toggle', requireRoles(...ROLES), ctrl.toggleActive);
+router.patch('/:id', requireRoles(...ADMIN_ROLES), ctrl.update);
+router.patch('/:id/toggle', requireRoles(...ADMIN_ROLES), ctrl.toggleActive);
 
 module.exports = router;
